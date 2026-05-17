4140c76b61b01bdea2592444fe4cd9ee3a1e9dea
Krish Bharucha <krishbharucha03@gmail.com>
2026-05-17 12:20:48 -0700

    about update

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -14,20 +14,18 @@ export const About = () => {
           <div className="h-1 w-28 mx-auto bg-gradient-to-r from-[#5c9ead] via-purple-500 to-pink-500 rounded-full mb-6 animate-pulse" />
 
           <p className="text-xl text-gray-400 max-w-3xl mx-auto">
-            Passionate about AI and data science to solve real-world problems
+            Passionate about AI and Data Science to solve real-world problems
           </p>
         </div>
 
         <div className="grid lg:grid-cols-2 gap-12 items-center">
           <div className="space-y-6">
             <p className="text-lg text-gray-300 leading-relaxed">
-              I am an aspiring AI Developer, Data Scientist, Business Analyst and Product Manager with a strong foundation in Artificial Intelligence, Machine Learning, 
-              Data pipelines and Scrum. 
-              As a Master’s student in Information Management at the University of Washington, Seattle, I bring a unique blend of technical expertise, research-driven innovation, and product-oriented thinking.
+              I am an aspiring MLE and AI Software Engineer.
+              
+As a Master’s student in Information Management at the University of Washington, Seattle, I bring a unique blend of technical expertise, research-driven innovation, and product-oriented thinking.
 
-My technical skills span across Python, JavaScript, C++, SQL, TensorFlow, OpenCV, NLP, Docker, and cloud platforms like AWS, equipping me to build and deploy scalable AI solutions from the ground up. I have worked extensively with Large Language Models (LLMs), XAI techniques like SHAP, and multimodal learning frameworks, and am passionate about making AI systems explainable, responsible, and aligned with real-world use cases.
-
-I help organizations translate complex data into actionable insights, enhance decision-making with intelligent automation, and drive innovation through AI-powered products. With a solid understanding of both technology and strategy, I bring cross-functional collaboration, agile execution, and systems thinking to every project, making me an ideal fit for roles in artificial intelligence, data science, and technical product and program management.
+My technical expertise spans Python, OpenCV, NLP, Docker, and cloud platforms such as Azure, enabling me to design, build, and deploy scalable AI solutions from the ground up. I have extensive experience working with Large Language Models (LLMs), HFTs, and multimodal learning frameworks.
 
 Driven by curiosity, purpose, and impact, I aim to build solutions that don’t just support AI but do so with clarity, fairness, and long-term business value. I am always excited to collaborate with forward-thinking teams who are reimagining the future of technology.
             </p>
